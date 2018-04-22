b9d1d9d186ea38319afba5e9626803a1b52025ad
SilencerWeb <silencer.web@gmail.com>
2018-04-22 09:33:08 +0500

    Initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,47 @@
+const reels = [
+  {
+    name: 'wild',
+    score: [100, 10, 100],
+  },
+  {
+    name: 'star',
+    score: [90, 9, 18],
+  },
+  {
+    name: 'bell',
+    score: [80, 8, 16],
+  },
+  {
+    name: 'shell',
+    score: [70, 7, 14],
+  },
+  {
+    name: 'seven',
+    score: [60, 6, 12],
+  },
+  {
+    name: 'cherry',
+    score: [50, 5, 10],
+  },
+  {
+    name: 'bar',
+    score: [40, 4, 8],
+  },
+  {
+    name: 'king',
+    score: [30, 3, 6],
+  },
+  {
+    name: 'queen',
+    score: [20, 2, 4],
+  },
+  {
+    name: 'jack',
+    score: [10, 1, 2],
+  },
+];
+
+
+const generateReels = () => {
+
+};
