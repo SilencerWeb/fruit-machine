b1fe864af5452c44a91ee3598091f5712be7b2a5
SilencerWeb <silencer.web@gmail.com>
2018-04-26 16:59:17 +0500

    Add basic UI without styles

--- src/index.js
+++ src/index.js
@@ -9,14 +9,42 @@ import 'normalize.css/normalize.css';
 import './assets/styles/main.css';
 
 const FruitMachine = function () {
+  this.state = {
+    reels: [],
+    spin: [],
+    score: null,
+  };
+
+  const handleSpinButtonClick = (e) => {
+    e.preventDefault();
+
+    const state = this.state;
+
+
+    const reels = getShuffledReelsList();
+    const spin = this.spin(reels);
+    const score = this.getSpinScore(reels, spin);
+
+
+    this.state = Object.assign(state, {
+      reels: reels,
+      spin: spin,
+      score: score,
+    });
+
+
+    this.updateUI();
+  };
+
   this.spin = (reels) => {
     return reels.map(() => {
-      return generateRandomNumber(1, 10);
+      return generateRandomNumber(1, reels.length);
     });
   };
 
   this.getSpinScore = (reels, spins) => {
     let score = 0;
+
 
     const spinResultReels = {};
 
@@ -25,6 +53,7 @@ const FruitMachine = function () {
 
       spinResultReels[reel] = spinResultReels[reel] ? spinResultReels[reel] + 1 : 1;
     });
+
 
     const matchReels = Object.keys(spinResultReels);
 
@@ -46,15 +75,88 @@ const FruitMachine = function () {
       }
     }
 
+
     return score;
+  };
+
+  this.drawUI = (reelsAmount = 3) => {
+    const state = this.state;
+
+
+    const fruitMachine = document.querySelector('.fruit-machine');
+
+
+    const reelsWrapper = document.createElement('div');
+    reelsWrapper.classList.add('reels-wrapper');
+
+
+    for (let i = 0; i < reelsAmount; i++) {
+      const reel = document.createElement('div');
+      reel.classList.add('reel');
+
+      if (state.reels[i] && state.spin[i]) {
+        reel.textContent = state.reels[i][state.spin[i]];
+      }
+
+      reelsWrapper.appendChild(reel);
+    }
+
+
+    const spinButton = document.createElement('button');
+    spinButton.classList.add('spin-button');
+    spinButton.textContent = 'Spin!';
+
+    spinButton.addEventListener('click', handleSpinButtonClick);
+
+
+    const score = document.createElement('span');
+    score.classList.add('score');
+    score.textContent = 'Press \'Spin!\'';
+
+
+    fruitMachine.appendChild(reelsWrapper);
+    fruitMachine.appendChild(score);
+    fruitMachine.appendChild(spinButton);
+  };
+
+  this.updateUI = () => {
+    const state = this.state;
+
+
+    const fruitMachine = document.querySelector('.fruit-machine');
+    const reels = fruitMachine.querySelectorAll('.reel');
+    const spinButton = fruitMachine.querySelector('.spin-button');
+    const score = fruitMachine.querySelector('.score');
+
+
+    reels.forEach((reel, i) => {
+      reel.textContent = state.reels[i][state.spin[i]];
+    });
+
+
+    score.textContent = `Your score: ${state.score}`;
+  };
+
+  this.start = () => {
+    const state = this.state;
+
+
+    const reels = getShuffledReelsList();
+    const spin = this.spin(reels);
+    const score = this.getSpinScore(reels, spin);
+
+
+    this.state = Object.assign(state, {
+      reels: reels,
+      spin: spin,
+      score: score,
+    });
+
+
+    this.drawUI();
   };
 };
 
 const fruitMachine = new FruitMachine();
 
-const reels = getShuffledReelsList();
-const spin = fruitMachine.spin(reels);
-const score = fruitMachine.getSpinScore(reels, spin);
-
-console.log(reels, spin);
-console.log(`Your score: ${score}`);
+fruitMachine.start();
