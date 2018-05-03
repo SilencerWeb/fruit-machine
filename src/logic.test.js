78f95753765d9e6affc9ebbbd5ff0b71baf156b1
SilencerWeb <silencer.web@gmail.com>
2018-05-03 14:37:33 +0500

    Move FruitMachine from index.js to logic.js; Add tests; Add .babelrc; Change scripts in package.json; Add dependency jest

---
+++ src/logic.test.js
@@ -0,0 +1,27 @@
+import { FruitMachine } from './logic';
+
+const fruitMachine = new FruitMachine();
+
+describe('should pass spin tests', () => {
+  test('3 same reels', () => {
+    expect(fruitMachine.spin(['wild', 'wild', 'wild'])).toEqual(1000);
+    expect(fruitMachine.spin(['cherry', 'cherry', 'cherry'])).toEqual(500);
+  });
+
+  test('2 same reels', () => {
+    expect(fruitMachine.spin(['wild', 'wild', 'star'])).toEqual(100);
+    expect(fruitMachine.spin(['cherry', 'king', 'cherry'])).toEqual(50);
+    expect(fruitMachine.spin(['jack', 'queen', 'queen'])).toEqual(20);
+  });
+
+  test('2 same reels and wild', () => {
+    expect(fruitMachine.spin(['king', 'king', 'wild'])).toEqual(60);
+    expect(fruitMachine.spin(['bar', 'wild', 'bar'])).toEqual(80);
+    expect(fruitMachine.spin(['wild', 'bell', 'bell'])).toEqual(160);
+  });
+
+  test('no same reels', () => {
+    expect(fruitMachine.spin(['king', 'cherry', 'bell'])).toEqual(0);
+    expect(fruitMachine.spin(['shell', 'wild', 'seven'])).toEqual(0);
+  });
+});
